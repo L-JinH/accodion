a44784d8a1247c6a2358ed31bc5ecb950d55997d
JH <89690357+L-JinH@users.noreply.github.com>
2022-01-26 17:00:34 +0900

    accodion-b

---
+++ accodion-study/accodion-b.js
@@ -0,0 +1,19 @@
+const question = document.querySelectorAll(".question");
+const header = document.querySelectorAll(".header");
+const body = document.querySelectorAll(".body");
+let bodyHeight = 0;
+
+for (let i = 0; i < header.length; i++) {
+  header[i].addEventListener("click", function (ev) {
+    // 적힌 순서 중요. 다 뺀 다음 추가.
+    removeActive();
+    this.parentNode.classList.add("active");
+  });
+}
+
+// active 전부 빼기.
+function removeActive() {
+  for (let j = 0; j < question.length; j++) {
+    question[j].classList.remove("active");
+  }
+}
